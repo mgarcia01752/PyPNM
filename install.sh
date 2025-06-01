0563dd8307806b192374b19a8fea5db52cf1d55f
Maurice Garcia <mgarcia01752@outlook.com>
2025-06-01 01:44:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -108,11 +108,11 @@ echo "✅ Generated 'mibs/index.md' with a list of all .txt files."
 # ──────────────────────────────────────────────────────────────────────────────
 echo
 echo "Setting up Python virtual environment…"
-"$PYTHON_PKG" -m venv .venv
+"$PYTHON_PKG" -m venv .env
 
 # Activate the venv in this script context
 # (for user interactive shells they'll need to `source .venv/bin/activate`)
-source .venv/bin/activate
+source .env/bin/activate
 
 echo "Upgrading pip, setuptools, and wheel in venv…"
 pip install --upgrade pip setuptools wheel
