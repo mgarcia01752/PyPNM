a9e5acfcd810bee4471b7fa01bdb2bd12b5f6180
Maurice Garcia <mgarcia01752@outlook.com>
2025-07-12 09:12:53 -0600

    Start DsChanEst

--- src/pypnm/api/routes/docs/pnm/ds/ofdm/chan_est_coeff/router.py
+++ src/pypnm/api/routes/docs/pnm/ds/ofdm/chan_est_coeff/router.py
@@ -52,12 +52,26 @@ Performs signal-domain analysis of DOCSIS 3.1 channel estimation coefficients. O
 
 📘 [API Guide – Get Analysis](https://github.com/mgarcia01752/PyPNM/blob/main/documentation/api/fast-api/single/ds/ofdm/channel-estimation.md#get-analysis)
 """
+
+        measurement_statistics_description = """
+**Summarize Downstream OFDM Channel Estimation Coefficient Statistics**
+
+This endpoint returns high-level channel estimation metrics per downstream OFDM channel, including:
+- Amplitude ripple (RMS, peak-to-peak)
+- Group delay ripple (RMS, peak-to-peak)
+- Amplitude slope, group delay slope, and mean values
+- Measurement status and associated binary filename
+
+📘 [API Guide](https://github.com/mgarcia01752/PyPNM/blob/main/documentation/api/fast-api/single/ds/ofdm/channel-estimation.md#get-measurement-statistics)
+"""
+
         super().__init__(
             prefix="/docs/pnm/ds/ofdm",
             tags=["PNM Operations - Downstream OFDM Channel Estimation"],
             base_endpoint="/channelEstCoeff",
             set_measurement_description = measurement_description,
-            set_analysis_description = analysis_description)
+            set_analysis_description = analysis_description,
+            set_measurement_statistics_description=measurement_statistics_description)
         self.logger = logging.getLogger("ChannelEstimationCoefficentRouter")
 
     async def get_measurement_logic(self, request: PnmRequest) -> Union[PnmMeasurementResponse, SnmpResponse]:
@@ -112,6 +126,32 @@ Performs signal-domain analysis of DOCSIS 3.1 channel estimation coefficients. O
                                       status=ServiceStatusCode.SUCCESS,
                                       data=analysis.get_results()) 
 
+    async def get_measurement_statistics_logic(self, request: PnmRequest) -> Union[SnmpResponse]:
+        """
+        Retrieves OFDM Channel Estimation Coefficient measurement statistics for a DOCSIS 3.1 cable modem.
+
+        This includes values such as amplitude ripple (RMS & peak-to-peak), group delay ripple,
+        group delay slope, and related channel estimation metrics across all downstream OFDM channels.
+        """
+        self.logger.info(f"Fetching OFDM Channel Estimation Coefficient Statistics for MAC: {request.mac_address}")
+
+        cm: CableModem = CableModem(MacAddress(request.mac_address), Inet(request.ip_address))
+
+        status, msg = await CableModemServicePreCheck(cable_modem=cm).run_precheck()
+        if status != ServiceStatusCode.SUCCESS:
+            self.logger.error(msg)
+            return SnmpResponse(
+                mac_address=str(request.mac_address),
+                status=status, message=msg)
+
+        service: CmDsOfdmChanEstCoefService = CmDsOfdmChanEstCoefService(cm)
+        service_measure_stat = await service.get_pnm_measurement_statistics()
+
+        return SnmpResponse(
+            mac_address=str(request.mac_address),
+            status=ServiceStatusCode.SUCCESS,
+            message="Measurement Statistics for OFDM Channel Estimation Coefficients",
+            results=service_measure_stat)
 
 # ✅ Required for dynamic auto-registration
 router = ChannelEstimationCoefficentRouter().router
