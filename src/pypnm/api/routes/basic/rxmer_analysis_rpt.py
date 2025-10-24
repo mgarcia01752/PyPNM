0bd673fee72ef7b1eb155548f0626c89417d0d2e
Maurice Garcia <mgarcia01752@outlook.com>
2025-10-24 11:15:52 -0600

    Next

--- src/pypnm/api/routes/basic/rxmer_analysis_rpt.py
+++ src/pypnm/api/routes/basic/rxmer_analysis_rpt.py
@@ -138,9 +138,14 @@ class RxMerAnalysisReport(AnalysisReport):
             try:
 
                 cfg = PlotConfig(
-                    title=f'{title_prefix}',
-                    x=x_khz, xlabel="Frequency",  x_tick_mode="mhz", x_mhz_from="mhz", x_tick_decimals=0,
-                    y_multi=[y_db, rl], y_multi_label=["RxMER", "Regression Line"],
+                    title           =   f'{title_prefix}',
+                    x               =   x_khz, 
+                    xlabel          =   "Frequency",  
+                    x_tick_mode     =   "mhz", 
+                    x_mhz_from      =   "khz", 
+                    x_tick_decimals =   0,
+                    y_multi         =   [y_db, rl], 
+                    y_multi_label   =   ["RxMER", "Regression Line"],
                     grid=True, legend=True, transparent=False, theme="dark")
 
                 multi = self.create_png_fname(tags=[str(channel_id), 'rxmer'])
